892240a4b07e9217d1890fabe7e7570334ff5f2f
ReformedMrozu <p.mroz.zywiol@gmail.com>
2021-03-13 15:36:31 +0100

    wip xd

--- build/asymnet.js
+++ build/asymnet.js
@@ -1,3 +1,23 @@
 // const confCols = require("../config.json").cols
 // const confRows = require("../config.json").rows
 
+var rows = $("#config-form").find('input[name="#rows_form"]').text()
+var cols = parseInt($("#cols_form").text())
+
+function UpdateStatBox() {
+    try {
+        // if (rows == null) {
+        //     rows = 7
+        // }
+        if (cols == NaN) {
+            cols = 4
+        }
+
+        $("#rows-stat-box").text(rows)
+        console.log(rows)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+// $("form").submit(UpdateStatBox());
